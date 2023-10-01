1ef7bd4365e51d5758078465d40072a8caeb84b0
Crowdbotics <team@crowdbotics.com>
2023-10-01 22:16:21 +0000

    Initial commit

---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "wffw-paye-43698",
+  url: "https://wffw-paye-43698.botics.co",
+  api: "https://wffw-paye-43698.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
